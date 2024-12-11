b7413c2ad97e8ce66aad2625e8e783c3ff896cb3
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-11 08:39:56 +0000

    Imported newly added components

--- components/ECommerceForm.js
+++ components/ECommerceForm.js
@@ -1,12 +1,16 @@
 import React, {Component} from "react"
 
 import Header from "./Header.js"
+import Content from "./Content.js"
+import Footer from "./Footer.js"
 
 export default class ECommerceForm extends Component {
     render() {
         return (
             <div>
                 <Header />
+                <Content />
+                <Footer />
             </div>
         )
     }
