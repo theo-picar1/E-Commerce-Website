f8fc0e3933fbdaa83989237a84cd798c2247ce4f
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-11 08:39:27 +0000

    New Footer component

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer id="footer">  
+                <h1>Footer</h1>
+            </footer>         
+        )
+    }
+}
