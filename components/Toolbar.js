80a629c3e1609b659e9fa268bfa557ad3379b62c
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-14 18:59:55 +0000

    Completed content for Toolbar

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -4,21 +4,23 @@ export default class Toolbar extends Component {
     render() {
         return (
             <div id="toolbar">
-                <div id="left-content">
-                    <div id="website-title">
-                        <h1 id="sound-h1">SOUND</h1>
-                        <h1 id="bizzare-h1">BIZZARE</h1>
-                        <img src="/images/website-logo.jpg" className="website-logo"/>
+                <div id="toolbar-content">  
+                    <div id="left-content">
+                        <div id="website-title">
+                            <h1 id="sound-h1">SUPER</h1>
+                            <h1 id="bizzare-h1">MENTS</h1>
+                            <img src="/images/website-logo.jpg" className="website-logo"/>
+                        </div>
+                        <div id="searchbar-container">
+                            <input type="text" id="searchbar" placeholder="Search for instruments..."/>
+                            <img src="/images/search.png" className="website-icon"/>
+                        </div>
                     </div>
-                    <div id="searchbar-container">
-                        <input type="text" id="searchbar" placeholder="Search for instruments..."/>
-                        <img src="/images/search.png" className="website-icon"/>
-                    </div>
-                </div>
-                <div id="right-content">
-                    <div>
-                        <img src="/images/shopping-cart.png" className="website-icon"/>
-                        <img src="/images/user.png" className="website-icon"/>
+                    <div id="right-content">
+                        <div>
+                            <img src="/images/shopping-cart.png" className="website-icon"/>
+                            <img src="/images/user.png" className="website-icon"/>
+                        </div>
                     </div>
                 </div>
             </div> 
