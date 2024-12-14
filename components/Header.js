ec65b21b468c4c44938e125f92e374e27db4f4aa
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-14 18:59:09 +0000

    Changed around content of header

--- components/Header.js
+++ components/Header.js
@@ -4,19 +4,21 @@ export default class Header extends Component {
     render() {
         return (
             <header id="header">  
-                <div class="header-section">
-                    <img src="/images/telephone.png" className="website-icon"/>
-                    <p><b>+(353)85-253-1298</b> Round the clock free hotline (24/7)</p>
-                </div>
-                <div class="header-section">
-                    <img src="/images/clock.png" className="website-icon"/>
-                    <p>Hours: 7 days a week from 9AM to 5PM</p>
-                </div>
-                <div class="header-section" id="nav-tabs">
-                    <p>Home</p>
-                    <p>About Us</p>
-                    <p>Contacts</p>
-                    <p>Blog</p>
+                <div id="header-content">
+                    <div class="header-section">
+                        <img src="/images/telephone.png" className="website-icon"/>
+                        <p>Round-the-clock free hotline (24/7)</p>
+                    </div>
+                    <div class="header-section">
+                        <img src="/images/clock.png" className="website-icon"/>
+                        <p>7 days a week from 9AM to 5PM</p>
+                    </div>
+                    <div class="header-section" id="nav-tabs">
+                        <p>Home</p>
+                        <p>About Us</p>
+                        <p>Contacts</p>
+                        <p>Blog</p>
+                    </div>
                 </div>
             </header>         
         )
