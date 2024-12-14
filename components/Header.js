223a958dfa3e4fe737c62773f49822556a7ce605
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-14 08:31:20 +0000

    Cleaned up Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,20 @@ export default class Header extends Component {
     render() {
         return (
             <header id="header">  
-                <h1>E-Commerce Website</h1>
+                <div class="header-section">
+                    <img src="/images/telephone.png" className="website-icon"/>
+                    <p><b>+(353)85-253-1298</b> Round the clock free hotline (24/7)</p>
+                </div>
+                <div class="header-section">
+                    <img src="/images/clock.png" className="website-icon"/>
+                    <p>Hours: 7 days a week from 9AM to 5PM</p>
+                </div>
+                <div class="header-section" id="nav-tabs">
+                    <p>Home</p>
+                    <p>About Us</p>
+                    <p>Contacts</p>
+                    <p>Blog</p>
+                </div>
             </header>         
         )
     }
