f760f0b09bb1c62e00ae812089d5d80dd3241376
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-01-22 16:26:29 +0000

    Now using json data with fetch

--- components/home/FeaturedProducts.js
+++ components/home/FeaturedProducts.js
@@ -1,115 +1,61 @@
 import React, {Component} from 'react'
 
 export default class FeaturedProducts extends Component {
+    constructor(props)
+    {
+        super(props)
+
+        this.state = {
+            products: [],
+            originalProducts: [],
+            productKeys: []
+        }
+    }
+               
+    componentDidMount() {
+        let url = "json/products.json"
+
+        fetch(url)
+            .then(response => response.json())
+            .then(jsonData => {
+                let keys = []
+
+                jsonData.forEach(product => {
+                    // just to get all keys from products.json because first object might not have all the keys or different keys from other objects
+                    keys.push(...Object.keys(product))
+                })
+
+                // then get rid of duplicates, because I pushed EVERY single key and tag from EVERY product
+                let uniqueKeys = [...new Set(keys)]
+                
+                this.setState({
+                    products: jsonData,
+                    originalProducts: jsonData,
+                    productKeys: uniqueKeys,
+                })
+            })
+    }
+
     render() {
         return (
             <div id="featured-products-container">
                 <p className="subheading">FEATURED PRODUCTS</p>
                 <div id="featured-products-gallery">
-                    <div className="featured-product">
-                        <div className="product-image" style={{backgroundImage: `url(/images/sdp1-keyboard.webp)`}}>
-                            <div><p>&#77946;</p></div>
+                    {this.state.products.map(product => 
+                        <div className="featured-product">
+                            <div className="product-image" style={{backgroundImage: `url(${product["productImgs"][0]})`}}>
+                                <div><p>&#77946;</p></div>
+                            </div>
+                            <div className="product-details">
+                                <p className="product-name">{product["name"]}</p>
+                                <p className="product-price">€{product["price"]}</p>
+                                <button className="shop-now-button">
+                                    <img src="/images/shopping-cart.png"/>
+                                    <p>ADD TO CART</p>
+                                </button>
+                            </div>
                         </div>
-                        <div className="product-details">
-                            <p className="product-name">SDP-1 Portable Digital Keyboard Piano by Gear4music + Stand and Headphones</p>
-                            <p className="product-price">€197.25</p>
-                            <button className="shop-now-button">
-                                <img src="/images/shopping-cart.png"/>
-                                <p>ADD TO CART</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="featured-product">
-                        <div className="product-image" style={{backgroundImage: `url(/images/guitar-amplifier.webp)`}}>
-                            <div><p>&#77946;</p></div>
-                        </div>
-                        <div className="product-details">
-                            <p className="product-name">3rd Avenue 10W Guitar Amplifier with Headphone Output, Overdrive Switch, Tone Control–Portable Compact Practice Amp–Black</p>
-                            <p className="product-price">€42.18</p>
-                            <button className="shop-now-button">
-                                <img src="/images/shopping-cart.png"/>
-                                <p>ADD TO CART</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="featured-product">
-                        <div className="product-image" style={{backgroundImage: `url(/images/donner-amplifier.webp)`}}>
-                            <div><p>&#77946;</p></div>
-                        </div>
-                        <div className="product-details">
-                            <p className="product-name">Donner Bluetooth 5.0 Stereo Audio Amplifier Receiver, Peak power 440 W Hi-Fi Wireless 4 channel Desktop Amp with USB, SD,FM, 2 Mic IN Echo, RCA, LED, Speaker Selector for Studio, Home Speakers-MAMP5</p>
-                            <p className="product-price">€130.38</p>
-                            <button className="shop-now-button">
-                                <img src="/images/shopping-cart.png"/>
-                                <p>ADD TO CART</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="featured-product">
-                        <div className="product-image" style={{backgroundImage: `url(/images/junior5-drums.webp)`}}>
-                            <div><p>&#77946;</p></div>
-                        </div>
-                        <div className="product-details">
-                            <p className="product-name">Junior 5 Piece Drum Kit by Gear4music, Blue</p>
-                            <p className="product-price">€209.50</p>
-                            <button className="shop-now-button">
-                                <img src="/images/shopping-cart.png"/>
-                                <p>ADD TO CART</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="featured-product">
-                        <div className="product-image" style={{backgroundImage: `url(/images/gibson-guitar.jpg)`}}>
-                            <div><p>&#77946;</p></div>
-                        </div>
-                        <div className="product-details">
-                            <p className="product-name">Gibson Les Paul Standard '50s Tribute</p>
-                            <p className="product-price">€2290.00</p>
-                            <button className="shop-now-button">
-                                <img src="/images/shopping-cart.png"/>
-                                <p>ADD TO CART</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="featured-product">
-                        <div className="product-image" style={{backgroundImage: `url(/images/bdk1-plus-drums.webp)`}}>
-                            <div><p>&#77946;</p></div>
-                        </div>
-                        <div className="product-details">
-                            <p className="product-name">BDK-1plus Full Size Starter Drum Kit by Gear4music, Blue</p>
-                            <p className="product-price">€370.00</p>
-                            <button className="shop-now-button">
-                                <img src="/images/shopping-cart.png"/>
-                                <p>ADD TO CART</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="featured-product">
-                        <div className="product-image" style={{backgroundImage: `url(/images/k1-keyboard.webp)`}}>
-                            <div><p>&#77946;</p></div>
-                        </div>
-                        <div className="product-details">
-                            <p className="product-name">K1 61-Note Keyboard by Gear4music</p>
-                            <p className="product-price">€61.60</p>
-                            <button className="shop-now-button">
-                                <img src="/images/shopping-cart.png"/>
-                                <p>ADD TO CART</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="featured-product">
-                        <div className="product-image" style={{backgroundImage: `url(/images/grv-rock-drums.jpeg)`}}>
-                            <div><p>&#77946;</p></div>
-                        </div>
-                        <div className="product-details">
-                            <p className="product-name">GRV Square Rock Drum Set (BK) + Cymbals Set B20</p>
-                            <p className="product-price">€684.55</p>
-                            <button className="shop-now-button">
-                                <img src="/images/shopping-cart.png"/>
-                                <p>ADD TO CART</p>
-                            </button>
-                        </div>
-                    </div>
+                    )}
                 </div>
             </div>
         )
