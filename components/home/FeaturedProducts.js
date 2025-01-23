ca6d3755575d507b090396fc7adfd87ec3a21fc3
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-01-23 11:05:00 +0000

    Changed layout of image slideshow

--- components/home/FeaturedProducts.js
+++ components/home/FeaturedProducts.js
@@ -37,18 +37,23 @@ export default class FeaturedProducts extends Component {
     }
 
     render() {
+
         return (
             <div id="featured-products-container">
                 <p className="subheading">FEATURED PRODUCTS</p>
                 <div id="featured-products-gallery">
                     {this.state.products.map(product => 
-                        <div className="featured-product">
-                            <div className="product-image" style={{backgroundImage: `url(${product["productImgs"][0]})`}}>
-                                <div><p>&#77946;</p></div>
+                        <div className="featured-product" key={ product["id"] }>
+                            <div className="product-image-container">
+                                {product["productImgs"].map(img => 
+                                    <div className="product-image" key={ img } style={{backgroundImage: `url(${img})`}}>
+                                        <div><p>&#77946;</p></div>
+                                    </div>
+                                )}
                             </div>
                             <div className="product-details">
                                 <p className="product-name">{product["name"]}</p>
-                                <p className="product-price">€{product["price"]}</p>
+                                <p className="product-price">€{product["price"]}.99</p>
                                 <button className="shop-now-button">
                                     <img src="/images/shopping-cart.png"/>
                                     <p>ADD TO CART</p>
