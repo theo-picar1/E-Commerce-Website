819e5c32bd32d351b90d23ceebb73b41d3df248c
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-02-05 17:06:32 +0000

    Implemented basic filtering inputs

--- components/home/Filters.js
+++ components/home/Filters.js
@@ -1,21 +1,57 @@
 import React, {Component} from "react"
 
+import axios from 'axios'
+
+import { SERVER_HOST } from "../../config/global_constants"
+
 export default class Filters extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            price: ``,
+            minRating: ``,
+            maxRating: ``,
+            checkedInstruments: [],
+            minStock: ``,
+            maxStock: ``
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleCheckboxes = () => {
+        let checkboxes = document.getElementsByClassName("instrument-checkbox")
+
+        let selectedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value)
+
+        this.setState({
+            checkedInstruments: selectedCheckboxes
+        })
+    }
+
     render() {
         return (
             <div id="product-filters">
                 <p className="subheading">FILTERS</p>
                 <div className="filter-section">
                     <p className="filter-type-subheading">PRICE RANGE</p>
-                    <div className="section-content">
-                        <input type="range" min="1" max="5000" value="50"/>
+                    <div className="section-content"id="price-range-section">
+                        <p>€1</p>
+                        <input type="range" min="1" max="5000" id="price-range-slider" name="price" value={this.state.price} onChange={this.handleChange}/>
+                        <p>€5000+</p>
                     </div>
                 </div>
                 <div className="filter-section">
                     <p className="filter-type-subheading">RATING</p>
-                    <div className="section-content">
-                        <input type="text" placeholder="min"/>
-                        <input type="text" placeholder="min"/>
+                    <div className="section-content min-max-section">
+                        <input type="text" id="min-rating" placeholder="min" name="minRating" value={this.state.minRating} onChange={this.handleChange}/>
+                        <p>-</p>
+                        <input type="text" id="max-rating" placeholder="max" name="maxRating" value={this.state.maxRating} onChange={this.handleChange}/>
                     </div>
                 </div>
                 <div className="filter-section">
@@ -23,7 +59,7 @@ export default class Filters extends Component {
                     <div className="section-content" id="instrument-checkboxes">
                         { this.props.categories.map(category =>
                             <div>
-                                <input type="checkbox"/>
+                                <input type="checkbox" key={ category } className="instrument-checkbox" value={ category } onChange={ this.handleCheckboxes }/>
                                 <p>{category}</p>
                             </div>
                         )}
@@ -31,9 +67,10 @@ export default class Filters extends Component {
                 </div>
                 <div className="filter-section">
                     <p className="filter-type-subheading">STOCK QUANTITY</p>
-                    <div className="section-content">
-                        <input type="text" placeholder="min"/>
-                        <input type="text" placeholder="max"/>
+                    <div className="section-content min-max-section">
+                        <input type="text" placeholder="min" name="minStock" value={this.state.minStock} onChange={this.handleChange}/>
+                        <p>-</p>
+                        <input type="text" placeholder="max" name="maxStock" value={this.state.maxStock} onChange={this.handleChange}/>
                     </div>
                 </div>
             </div> 
