da8f12ce8c9aa358a2c5bd852963ff0de0e8edc8
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-02-06 14:29:30 +0000

    Removed unecessary header

--- App.js
+++ App.js
@@ -4,15 +4,12 @@ import { BrowserRouter, Switch, Route } from "react-router-dom/cjs/react-router-
 import "./css/App.css"
 import Home from "./components/Home"
 import NoSuchPage from "./components/NoSuchPage"
-import Header from "./components/Header"
 import Footer from "./components/Footer"
 
 export default class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Header />
-
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route path="/nosuchpage" component={NoSuchPage} />
