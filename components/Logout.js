7acd262eaf8b23ce39d716e53c17fd7f8225551b
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-02-13 14:32:54 +0000

    Created Logout.js file

---
+++ components/Logout.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react"
+import { Redirect } from "react-router-dom"
+import axios from "axios"
+
+import Button from "../components/Button"
+import { ACCESS_LEVEL_GUEST, SERVER_HOST } from "../config/global_constants"
+
+
+export default class Logout extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: true
+        }
+    }
+
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        axios.post(`${SERVER_HOST}/users/logout`).then(res => {
+            if (res.data) {
+                if (res.data.errorMessage) {
+                    console.log(res.data.errorMessage)
+                }
+                else {
+                    console.log("User logged out")
+                    sessionStorage.clear()
+
+                    sessionStorage.name = "GUEST"
+                    sessionStorage.accessLevel = ACCESS_LEVEL_GUEST
+                    this.setState({ isLoggedIn: false })
+                }
+            }
+            else {
+                console.log("Logout failed")
+            }
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                {!this.state.isLoggedIn ? <Redirect to="/DisplayAllCars" /> : null}
+
+                <Button value="Log out" className="red-button" onClick={this.handleSubmit} />
+            </div>
+        )
+    }
+}
