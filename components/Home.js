8e24aaa9ff2591c058049d6d74aa083812c035e8
osh-een <oisinbell09@gmail.com>
2025-02-17 23:55:30 +0000

    added ProductDetails.js and corresponding scss

--- components/Home.js
+++ components/Home.js
@@ -1,99 +1,142 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
 
-import Toolbar from "./Toolbar.js"
-import Filters from "./Filters.js"
-import ProductsGallery from "./ProductsGallery.js"
-import Footer from "./Footer.js"
+import Toolbar from "./Toolbar.js";
+import Filters from "./Filters.js";
+import ProductsGallery from "./ProductsGallery.js";
+import ProductDetails from "./ProductDetails.js";
+import Footer from "./Footer.js";
 
-import axios from "axios"
+import axios from "axios";
 
-import { SERVER_HOST } from "../config/global_constants.js"
+import { SERVER_HOST } from "../config/global_constants.js";
 
 export default class Home extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            products: [],
-            originalProducts: [],
-            categories: [],
-            cartCounter: 0,
-            sortBy: ``,
-            sortType: ``,
-            isLoggedIn: false,
-            isRegistered: false
-        }
-    }
+    this.state = {
+      products: [],
+      originalProducts: [],
+      categories: [],
+      cartCounter: 0,
+      sortBy: ``,
+      sortType: ``,
+      showProductDetails: false,
+      product: null,
+    };
+  }
 
-    componentDidMount() {
-        axios.get(`${SERVER_HOST}/products`).then(res => {
-            if(res.data) {
+  componentDidMount() {
+    axios.get(`${SERVER_HOST}/products`).then((res) => {
+      if (res.data) {
+        if (res.data.errorMessage) {
+          console.log(res.data.errorMessage);
+        } else {
+          console.log("Products have been successfully retrieved/read");
 
-                if(res.data.errorMessage) {
-                    console.log(res.data.errorMessage)
-                }
-                else {
-                    console.log("Products have been successfully retrieved/read")
+          let categories = [];
 
-                    let categories = []
-
-                    res.data.forEach(product => {
-                        if(product["category"]) {
-                            categories.push(product["category"])
-                        }
-                    })
-
-                    let uniqueCategories = [...new Set(categories)]
-                    
-                    this.setState({
-                        products: res.data,
-                        originalProducts: res.data,
-                        categories: uniqueCategories
-                    })
-                }
+          res.data.forEach((product) => {
+            if (product["category"]) {
+              categories.push(product["category"]);
             }
-        })
-    } 
-    
-    incrementCartCounter = () => {
-        this.setState(prevState => ({ cartCounter: prevState.cartCounter + 1 }))
-    }
+          });
 
-    sortProducts = (sortBy, sortType) => {
-        let updatedProducts = [...this.state.products]
+          let uniqueCategories = [...new Set(categories)];
 
-        if(sortType === "asc") {
-            updatedProducts.sort((a, b) => a[sortBy] > b[sortBy] ? 1 : -1)
+          this.setState({
+            products: res.data,
+            originalProducts: res.data,
+            categories: uniqueCategories,
+          });
         }
-        else if(sortType === "desc") {
-            updatedProducts.sort((a, b) => a[sortBy] < b[sortBy] ? 1 : -1)
-        }
+      }
+    });
+  }
 
-        this.setState({
-            products: updatedProducts
-        })
+  incrementCartCounter = () => {
+    this.setState((prevState) => ({ cartCounter: prevState.cartCounter + 1 }));
+  };
+
+  sortProducts = (sortBy, sortType) => {
+    let updatedProducts = [...this.state.products];
+
+    if (sortType === "asc") {
+      updatedProducts.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
+    } else if (sortType === "desc") {
+      updatedProducts.sort((a, b) => (a[sortBy] < b[sortBy] ? 1 : -1));
     }
 
-    setSortAttribute = (e) => {
-        let array = e.target.value.split('-')
+    this.setState({
+      products: updatedProducts,
+    });
+  };
 
-        let sortBy = array[0]
-        let sortType = array[1]
+  setSortAttribute = (e) => {
+    let array = e.target.value.split("-");
 
-        this.sortProducts(sortBy, sortType)
-    }
+    let sortBy = array[0];
+    let sortType = array[1];
 
-    
-    render() {
-        return (
-            <div className="page-content">
-                <Toolbar cartCounter={ this.state.cartCounter }/>
-                <div id="main-content">
-                    <Filters categories={ this.state.categories }/>
-                    <ProductsGallery products={ this.state.products } onClick={ this.incrementCartCounter } onSort={ this.setSortAttribute }/>
-                </div>
-                <Footer />
-            </div>
-        )
-    }
+    this.sortProducts(sortBy, sortType);
+  };
+
+  // Function to scroll to the top
+  scrollToTop = () => {
+    const topOfPage = document.getElementById("top-of-page");
+    topOfPage.scrollIntoView({ behavior: "smooth" });
+  };
+
+  handleProductClick = (product) => {
+    this.setState(
+      {
+        product,
+        showProductDetails: true,
+      },
+      () => {
+        this.scrollToTop();
+      }
+    );
+  };
+
+  closeProductDetails = () => {
+    this.setState({ showProductDetails: false, product: null });
+  };
+
+  render() {
+    const { showProductDetails, cartCounter, categories, products, product } =
+      this.state;
+    const {
+      incrementCartCounter,
+      setSortAttribute,
+      handleProductClick,
+      closeProductDetails,
+    } = this;
+    return (
+      <div className="page-content">
+        <div id="top-of-page"></div>
+        <Toolbar cartCounter={cartCounter} />
+        <div id="main-content">
+          {showProductDetails ? (
+            <ProductDetails
+              product={product}
+              closeProductDetails={closeProductDetails}
+              incrementCartCounter={incrementCartCounter}
+            />
+          ) : (
+            <>
+              <Filters categories={categories} />
+              <ProductsGallery
+                products={products}
+                incrementCartCounter={incrementCartCounter}
+                onSort={setSortAttribute}
+                handleProductClick={handleProductClick}
+              />
+            </>
+          )}
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
--- components/ProductsGallery.js
+++ components/ProductsGallery.js
@@ -1,42 +1,52 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 export default class FeaturedProducts extends Component {
-    render() {
-        return (
-            <div id="products-gallery-container">
-                <div id="products-gallery-header">
-                    <p className="subheading">FEATURED PRODUCTS</p>
-                    <select onChange={ this.props.onSort }>
-                        <option value="name-asc">Name (A-Z)</option>
-                        <option value="name-desc">Name (Z-A)</option>
-                        <option value="price-desc">Highest Price</option>
-                        <option value="price-asc">Lowest Price</option>
-                        <option value="rating-desc">Highest Rating</option>
-                        <option value="rating-asc">Lowest Rating</option>
-                    </select>
-                </div>
-                <div id="products-gallery">
-                    {this.props.products.map(product => 
-                        <div className="product" key={ product["id"] }>
-                            <div className="product-image-container">
-                                {product["productImgs"].map(img => 
-                                    <div className="product-image" key={ img } style={{backgroundImage: `url(${img})`}}>
-                                        <div><p>&#77946;</p></div>
-                                    </div>
-                                )}
-                            </div>
-                            <div className="product-details">
-                                <p className="product-name">{product["name"]}</p>
-                                <p className="product-price">€{product["price"]}.99</p>
-                                <button className="shop-now-button" onClick={this.props.onClick}>
-                                    <img src="/images/shopping-cart.png"/>
-                                    <p>ADD TO CART</p>
-                                </button>
-                            </div>
-                        </div>
-                    )}
-                </div>
+  render() {
+    return (
+      <div id="products-gallery-container">
+        <div id="products-gallery-header">
+          <p className="subheading">FEATURED PRODUCTS</p>
+          <select onChange={this.props.onSort}>
+            <option value="name-asc">Name (A-Z)</option>
+            <option value="name-desc">Name (Z-A)</option>
+            <option value="price-desc">Highest Price</option>
+            <option value="price-asc">Lowest Price</option>
+            <option value="rating-desc">Highest Rating</option>
+            <option value="rating-asc">Lowest Rating</option>
+          </select>
+        </div>
+        <div id="products-gallery">
+          {this.props.products.map((product) => (
+            <div className="product" key={product["id"]}>
+              <div className="product-image-container">
+                {product["productImgs"].map((img) => (
+                  <div
+                    className="product-image"
+                    key={img}
+                    style={{ backgroundImage: `url(${img})` }}
+                    onClick={() => this.props.handleProductClick(product)}
+                  >
+                    <div>
+                      <p>&#77946;</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="product-details">
+                <p className="product-name">{product["name"]}</p>
+                <p className="product-price">€{product["price"]}.99</p>
+                <button
+                  className="shop-now-button"
+                  onClick={this.props.incrementCartCounter}
+                >
+                  <img src="/images/shopping-cart.png" />
+                  <p>ADD TO CART</p>
+                </button>
+              </div>
             </div>
-        )
-    }
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
