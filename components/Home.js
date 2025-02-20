9a331554000d79d9373848cd0bc10fc6c8cc1112
lareishim <laremgmt@gmail.com>
2025-02-20 12:53:17 +0000

    implemented search

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import Toolbar from "./Toolbar.js";
 import Filters from "./Filters.js";
@@ -8,135 +8,149 @@ import Footer from "./Footer.js";
 
 import axios from "axios";
 
-import { SERVER_HOST } from "../config/global_constants.js";
+import {SERVER_HOST} from "../config/global_constants.js";
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      products: [],
-      originalProducts: [],
-      categories: [],
-      cartCounter: 0,
-      sortBy: ``,
-      sortType: ``,
-      showProductDetails: false,
-      product: null,
-    };
-  }
-
-  componentDidMount() {
-    axios.get(`${SERVER_HOST}/products`).then((res) => {
-      if (res.data) {
-        if (res.data.errorMessage) {
-          console.log(res.data.errorMessage);
-        } else {
-          console.log("Products have been successfully retrieved/read");
-
-          let categories = [];
-
-          res.data.forEach((product) => {
-            if (product["category"]) {
-              categories.push(product["category"]);
-            }
-          });
-
-          let uniqueCategories = [...new Set(categories)];
-
-          this.setState({
-            products: res.data,
-            originalProducts: res.data,
-            categories: uniqueCategories,
-          });
-        }
-      }
-    });
-  }
-
-  incrementCartCounter = () => {
-    this.setState((prevState) => ({ cartCounter: prevState.cartCounter + 1 }));
-  };
-
-  sortProducts = (sortBy, sortType) => {
-    let updatedProducts = [...this.state.products];
-
-    if (sortType === "asc") {
-      updatedProducts.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
-    } else if (sortType === "desc") {
-      updatedProducts.sort((a, b) => (a[sortBy] < b[sortBy] ? 1 : -1));
+        this.state = {
+            products: [],
+            originalProducts: [],
+            categories: [],
+            cartCounter: 0,
+            sortBy: ``,
+            sortType: ``,
+            showProductDetails: false,
+            product: null,
+            searchValue: "",
+        };
     }
 
-    this.setState({
-      products: updatedProducts,
-    });
-  };
+    componentDidMount() {
+        axios.get(`${SERVER_HOST}/products`).then((res) => {
+            if (res.data) {
+                if (res.data.errorMessage) {
+                    console.log(res.data.errorMessage);
+                } else {
+                    console.log("Products have been successfully retrieved/read");
 
-  setSortAttribute = (e) => {
-    let array = e.target.value.split("-");
+                    let categories = [];
 
-    let sortBy = array[0];
-    let sortType = array[1];
+                    res.data.forEach((product) => {
+                        if (product["category"]) {
+                            categories.push(product["category"]);
+                        }
+                    });
 
-    this.sortProducts(sortBy, sortType);
-  };
+                    let uniqueCategories = [...new Set(categories)];
 
-  // Function to scroll to the top
-  scrollToTop = () => {
-    const topOfPage = document.getElementById("top-of-page");
-    topOfPage.scrollIntoView({ behavior: "smooth" });
-  };
+                    this.setState({
+                        products: res.data,
+                        originalProducts: res.data,
+                        categories: uniqueCategories,
+                    });
+                }
+            }
+        });
+    }
 
-  handleProductClick = (product) => {
-    this.setState(
-      {
-        product,
-        showProductDetails: true,
-      },
-      () => {
-        this.scrollToTop();
-      }
-    );
-  };
+    incrementCartCounter = () => {
+        this.setState((prevState) => ({cartCounter: prevState.cartCounter + 1}));
+    };
 
-  closeProductDetails = () => {
-    this.setState({ showProductDetails: false, product: null });
-  };
+    sortProducts = (sortBy, sortType) => {
+        let updatedProducts = [...this.state.products];
 
-  render() {
-    const { showProductDetails, cartCounter, categories, products, product } =
-      this.state;
-    const {
-      incrementCartCounter,
-      setSortAttribute,
-      handleProductClick,
-      closeProductDetails,
-    } = this;
-    return (
-      <div className="page-content">
-        <div id="top-of-page"></div>
-        <Toolbar cartCounter={cartCounter} />
-        <div id="main-content">
-          {showProductDetails ? (
-            <ProductDetails
-              product={product}
-              closeProductDetails={closeProductDetails}
-              incrementCartCounter={incrementCartCounter}
-            />
-          ) : (
-            <>
-              <Filters categories={categories} />
-              <ProductsGallery
-                products={products}
-                incrementCartCounter={incrementCartCounter}
-                onSort={setSortAttribute}
-                handleProductClick={handleProductClick}
-              />
-            </>
-          )}
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+        if (sortType === "asc") {
+            updatedProducts.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
+        } else if (sortType === "desc") {
+            updatedProducts.sort((a, b) => (a[sortBy] < b[sortBy] ? 1 : -1));
+        }
+
+        this.setState({
+            products: updatedProducts,
+        });
+    };
+
+    setSortAttribute = (e) => {
+        let array = e.target.value.split("-");
+
+        let sortBy = array[0];
+        let sortType = array[1];
+
+        this.sortProducts(sortBy, sortType);
+    };
+
+    // Function to scroll to the top
+    scrollToTop = () => {
+        const topOfPage = document.getElementById("top-of-page");
+        topOfPage.scrollIntoView({behavior: "smooth"});
+    };
+
+    handleProductClick = (product) => {
+        this.setState(
+            {
+                product,
+                showProductDetails: true,
+            },
+            () => {
+                this.scrollToTop();
+            }
+        );
+    };
+
+    closeProductDetails = () => {
+        this.setState({showProductDetails: false, product: null});
+    };
+
+    handleSearch = (searchValue) => {
+        searchValue = searchValue.toLowerCase();
+        const filteredProducts = this.state.originalProducts.filter((product) =>
+            product.name.toLowerCase().includes(searchValue)
+        );
+
+        this.setState({
+            searchValue,
+            products: filteredProducts,
+        });
+    };
+
+    render() {
+        const {searchValue, showProductDetails, cartCounter, categories, products, product} =
+            this.state;
+        const {
+            incrementCartCounter,
+            setSortAttribute,
+            handleProductClick,
+            closeProductDetails,
+            handleSearch,
+        } = this;
+        return (
+            <div className="page-content">
+                <div id="top-of-page"></div>
+                <Toolbar cartCounter={cartCounter} onSearch={this.handleSearch} searchValue={searchValue}/>
+                <div id="main-content">
+                    {showProductDetails ? (
+                        <ProductDetails
+                            product={product}
+                            closeProductDetails={closeProductDetails}
+                            incrementCartCounter={incrementCartCounter}
+                        />
+                    ) : (
+                        <>
+                            <Filters categories={categories}/>
+                            <ProductsGallery
+                                products={products}
+                                incrementCartCounter={incrementCartCounter}
+                                onSort={setSortAttribute}
+                                handleProductClick={handleProductClick}
+                            />
+                        </>
+                    )}
+                </div>
+                <Footer/>
+            </div>
+        );
+    }
 }
--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,14 +1,16 @@
-import React, {Component} from "react"
-
-import { Link } from "react-router-dom"
-
-import { ACCESS_LEVEL_GUEST } from "../config/global_constants"
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import {ACCESS_LEVEL_GUEST} from "../config/global_constants";
 
 export default class Toolbar extends Component {
+    handleSearchChange = (e) => {
+        this.props.onSearch(e.target.value);
+    };
+
     render() {
         return (
             <div id="toolbar">
-                <div id="toolbar-content">  
+                <div id="toolbar-content">
                     <div id="website-title">
                         <div id="superments">
                             <p className="title" id="super">SUPER</p>
@@ -19,22 +21,29 @@ export default class Toolbar extends Component {
                         </div>
                     </div>
                     <div id="searchbar-container">
-                        <input type="text" id="searchbar" placeholder="What are you searching for?" autoComplete="off"/>
+                        <input
+                            type="text"
+                            id="searchbar"
+                            placeholder="What are you searching for?"
+                            autoComplete="off"
+                            value={this.props.searchValue}
+                            onChange={this.handleSearchChange}
+                        />
                         <img src="/images/search.png" className="website-icon"/>
                     </div>
                     <div id="user-tools">
                         <div id="shopping-cart-button">
                             <img src="/images/shopping-cart.png"/>
-                            <p>{ this.props.cartCounter }</p>
+                            <p>{this.props.cartCounter}</p>
                         </div>
-                        {sessionStorage.accessLevel > ACCESS_LEVEL_GUEST ? 
+                        {sessionStorage.accessLevel > ACCESS_LEVEL_GUEST ? (
                             <Link to={"/logout"}>Log out</Link>
-                        :
+                        ) : (
                             <Link to={"/login"}>Sign in</Link>
-                        }
+                        )}
                     </div>
                 </div>
-            </div> 
-        )
+            </div>
+        );
     }
 }
